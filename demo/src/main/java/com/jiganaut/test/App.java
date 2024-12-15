0a8d85726c420181f8f4087632c3c6ef87c10c08
junjimikami <jmikami@jiganaut.com>
2024-12-15 22:23:14 +0900

    Add initial Maven project structure with sample application and unit tests

---
+++ demo/src/main/java/com/jiganaut/test/App.java
@@ -0,0 +1,13 @@
+package com.jiganaut.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
